76b611de0a6bd24686d6545a5256ba309317e694
İsmail Caner <126804265+ismailcaner@users.noreply.github.com>
2024-07-13 19:47:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,99 @@
+let db;
+// WebAssembly modülünü başlat ve veritabanını oluştur
+window.initSqlJs({
+    locateFile: file => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.6.1/${file}`
+}).then(SQL => {
+    // Veritabanını oluşturun
+    db = new SQL.Database();
+    
+    // Veritabanı içeriğini localStorage'dan yükle
+    const savedDb = localStorage.getItem('myDatabase');
+    if (savedDb) {
+        db = new SQL.Database(new Uint8Array(JSON.parse(savedDb)));
+    } else {
+        // Tablo oluştur
+        db.run("CREATE TABLE contacts (id INTEGER PRIMARY KEY, name TEXT, email TEXT, start_date TEXT, end_date TEXT);");
+    }
+
+    // Form verilerini işleme
+    document.getElementById('dataForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Formun varsayılan olarak gönderilmesini engelle
+        
+        // Formdan veri al
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const startDate = document.getElementById('start-date').value;
+        const endDate = document.getElementById('end-date').value;
+
+        // Veriyi tabloya ekleyin
+        db.run("INSERT INTO contacts (name, email, start_date, end_date) VALUES (?, ?, ?, ?)", [name, email, startDate, endDate]);
+        
+        // Formu temizle
+        document.getElementById('dataForm').reset();
+        
+        // Veritabanını localStorage'a kaydet
+        saveDatabase();
+        
+        // Verileri yeniden görüntüleyin
+        displayData();
+    });
+
+    // Veritabanını localStorage'a kaydetme
+    function saveDatabase() {
+        const data = db.export();
+        localStorage.setItem('myDatabase', JSON.stringify(Array.from(new Uint8Array(data))));
+    }
+
+   
+    function displayData() {
+        const res = db.exec("SELECT * FROM contacts");
+        const outputContainer = document.getElementById("output");
+        outputContainer.innerHTML = '';
+        if (res.length) {
+            res[0].values.forEach(row => {
+
+                const id = row[0];
+                const name = row[1];
+                const email = row[2];
+                const startDate = row[3];
+                const endDate = row[4];
+
+                const nowdate = new Date();
+                const enddate = new Date(row[4]);  
+                const islem = enddate - nowdate;
+                const fark = Math.ceil(islem / (1000 * 3600 * 24));
+                
+                const div = document.createElement("div");
+                div.innerHTML = `
+                <div class="deneme">
+                    <div class="deneme1">
+                        <span id="name-${id}">${name}</span>
+                        <button class="btn-delete" data-id="${id}">Sil</button>
+                    </div>
+                    <div class="deneme1">
+                        <span id="email-${id}"> ${email}</span>
+                        <span id="end-date-${id}">Bitiş Tarihi: ${fark}</span>
+                    </div>
+                </div>
+                `;
+                
+                div.querySelector('.btn-delete').addEventListener('click', () => deleteRow(id));
+                outputContainer.appendChild(div);
+            });
+        } else {
+            const span = document.createElement("span");
+            span.textContent = "Ürün yok";
+            outputContainer.appendChild(span);
+        }
+    }
+    
+    function deleteRow(id) {
+        db.run("DELETE FROM contacts WHERE id = ?", [id]);
+        saveDatabase();
+        displayData();
+    }
+    
+    displayData();
+}).catch(err => {
+    console.error('Failed to initialize SQL.js:', err);
+});
