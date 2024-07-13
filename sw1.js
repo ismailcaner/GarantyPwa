29e6625a446f8bc8b7053f57dc9867150e81c2ca
İsmail Caner <126804265+ismailcaner@users.noreply.github.com>
2024-07-13 20:06:08 +0300

    Create sw1.js

---
+++ sw1.js
@@ -0,0 +1,42 @@
+// Önbellek adı
+const CACHE_NAME = 'cache v2';
+const urlsToCache = [
+  '/',
+  '/index.css',
+  '/index.js',
+  '/manifest.json'
+];
+
+// Web sayfası yüklendiğinde önbelleği açar ve ağdan en son veriyi alır
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// İstekleri işler ve önce ağdan veri almaya çalışır, daha sonra önbellekte veri varsa bu veriyi kullanır
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function(response) {
+        // Ağ yanıtı varsa önbelleğe kaydet
+        const clonedResponse = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, clonedResponse);
+        });
+        return response;
+      })
+      .catch(function() {
+        // Ağ yanıtı yoksa önbellekten yanıtı al
+        return caches.match(event.request)
+          .then(function(response) {
+            if (response) {
+              return response;
+            }
+            throw new Error('No response found in cache.');
+          });
+      })
+  );
+});
